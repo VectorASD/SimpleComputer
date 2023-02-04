801639a7521dd18cc75501ea3b4874edc78f5cf3
Прилепа Михаил Константинович <ip111s19@csc.sibsutis.ru>
2023-02-04 11:20:41 +0700

    Добавлена проверка формата и сборки

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef const char* text;
+typedef const char *text;
 
-int main(int argc, char** args) {
-	printf("YEAH!\n");
-	return 0;
+int main(int argc, char **args) {
+    printf("YEAH!\n");
+    return 0;
 }
