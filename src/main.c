8ad3ac82b1c9250c0a784338a5f0097d4e48edfc
Прилепа Михаил Константинович <ip111s19@csc.sibsutis.ru>
2023-02-11 08:59:00 +0700

    Доделал Makefile, т.к. он не пересобирал изменения

--- src/main.c
+++ src/main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef unsigned char byte;
 typedef const char *text;
 
+byte memory[100];
+
+int sc_memoryInit() {
+	for (int i = 0; i < 100; i++) memory[i] = 0;
+	return 0;
+}
+
 int main(int argc, char **args) {
-    printf("YEAH!\n");
+	sc_memoryInit();
+    printf("YEAH3!\n");
     return 0;
 }
