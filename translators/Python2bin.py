d45fcf59634fbe98679cb7df747b95417ca843f8
Прилепа Михаил <vectorasd@vk.com>
2023-05-03 21:14:12 +0700

    Добавлена поддержка переменных, а также, теперь можно проворачивать цепочки операций из '=', '+=', '-=', '*=', '%=', '+', '-', '*', '/' и '%' символов

--- translators/Python2bin.py
+++ translators/Python2bin.py
@@ -55,6 +55,7 @@ def Parser(code):
     #print(e.context)
     RaiseSE(code, e.context, e.msg)
 
+augassign = ['+=', '-=', '/=', '*=', '%=']
 """
 Основная модель результатирующего кода:
 1 ячейка.) Всегда здесь будет располагаться JMP до программы
@@ -68,6 +69,15 @@ code_length.) Сама программа
 10.) READ <куда считываем>
 11.) WRITE <что печатаем>
 
+20.) LOAD <что загружаем в аккум>
+21.) STORE <куда загружаем аккум>
+
+30.) ADD <с чем складываем аккум>
+31.) SUB <с чем вычитаем аккум>
+32.) DIVIDE <с чем делим аккум>
+33.) MUL <с чем умножаем аккум>
+34.) MOD <с чем делим по модулю аккум>
+
 40.) JUMP <куда прыгнуть>
 43.) HALT
 """
@@ -78,6 +88,13 @@ def printer(codes):
     alt = "☘️ %s☘️ " % first if type(first) is str else first
     if code == 10: print("READ %s" % alt)
     elif code == 11: print("WRITE %s" % alt)
+    elif code == 20: print("LOAD %s" % alt)
+    elif code == 21: print("STORE %s" % alt)
+    elif code == 30: print("ADD %s" % alt)
+    elif code == 31: print("SUB %s" % alt)
+    elif code == 32: print("DIVIDE %s" % alt)
+    elif code == 33: print("MUL %s" % alt)
+    elif code == 34: print("MOD %s" % alt)
     elif code == 40: print("JUMP %s" % alt)
     elif code == 43: print("HALT")
     else: exit("printer: %s код не найден" % code)
@@ -103,7 +120,7 @@ def linker(state):
 
   mem = [0x4000] * limit
   mem[0] = encode(40, start_p)
-  for n, const in enumerate(consts, start_c): mem[n] = const & 0x7fff
+  for n, const in enumerate(consts, start_c): mem[n] = 0 if const is None else const & 0x7fff
   for n in range(start_r, start_p): mem[n] = 0
   for n, code in enumerate(codes, start_p):
     if len(code) == 1:
@@ -137,10 +154,11 @@ def compiler(code):
   codes = []
   def add(code, *data): codes.append([code] + list(data))
 
-  consts = [] # рассчитано только под числа
-  def new_const(data):
-    try: return "c%s" % consts.index(data)
-    except ValueError: pass
+  consts = [] # рассчитано только под числа и переменные
+  def new_const(data = None):
+    if data is not None:
+      try: return "c%s" % consts.index(data)
+      except ValueError: pass
     n = len(consts)
     consts.append(data)
     return "c%s" % n
@@ -156,6 +174,11 @@ def compiler(code):
     if n[0] == "r":
       n = int(n[1:])
       if n in range(len(regs)): regs[n] = False
+  vars = {} # накопитель переменных
+  def new_var(name):
+    try: return vars[name]
+    except KeyError: var = vars[name] = new_const()
+    return var
   
   def test_name(func, node, values = "NAME"):
     name = get_name(node)
@@ -183,13 +206,30 @@ def compiler(code):
     if len(childs) not in lens: error("%s: Ожидался размер потомства = %s, но встречено %s внутри ноды:\n  %s" % (func, lens, len(childs), repr(node)))
     return childs
 
-  expr_types = ("NUMBER", "print_stmt", "power", "atom")
+  expr_types = ("NUMBER", "NAME", "arith_expr", "term", "print_stmt", "power", "atom")
   def expr(node):
     name = get_name(node)
-    # Recurs(node)
     if name == "NUMBER":
       num = int(node.value)
       reg = new_const(num)
+    elif name == "NAME":
+      reg = new_var(node.value)
+    elif name in ("arith_expr", "term"): # arith_expr это '+' и '-';  term это '*', '/' и '%'
+      # Recurs(node)
+      for n, node in enumerate(node.children):
+        if n % 2:
+          let = node
+          continue
+        if n == 0:
+          reg = expr(node)
+          continue
+        reg2 = expr(node)
+        value = let.value
+        add(20, reg) # LOAD <reg>
+        add(augassign.index(value + "=") + 30, reg2) # ADD/SUB/DIVIDE/MUL/MOD <reg2>
+        add(21, reg) # STORE <reg>
+        free_reg(reg2)
+      # exit()
     elif name == "print_stmt":
       a, b = check_len("expr:print_stmt", node, 2)
       check_value("expr:print_stmt", a, "print")
@@ -213,11 +253,31 @@ def compiler(code):
     else: error("expr: Не известный тип:", name)
     return reg
   
+  def expr_stmt(node):
+    if len(node.children) % 2 != 1: error("expr_stmt: Размер expr_stmt чётный и равен", len(node.children))
+    right = node.children[-1]
+    reg = expr(right)
+    for n, node in enumerate(node.children[::-1][1:]):
+      if n % 2 == 0: let = node
+      else: left_expr_stmt(node, reg, let)
+    free_reg(reg)
+  def left_expr_stmt(node, reg, let = None):
+    check_name("left_expr_stmt", node) # пока поддерживаются NAME слева от =, +=, -= и т.д.
+    dst = new_var(node.value)
+    if let is None or let.value == "=":
+      add(20, reg) # LOAD <reg>
+      add(21, dst) # STORE <dst>
+    else:
+      add(20, dst) # LOAD <dst>
+      add(augassign.index(let.value) + 30, reg) # ADD/SUB/DIVIDE/MUL/MOD <reg>
+      add(21, dst) # STORE <dst>
+    
   def simple_stmt(node):
     for node in node.children:
       name = get_name(node)
       if name in ("NEWLINE", "SEMI"): continue
-      if name in expr_types:
+      if name == "expr_stmt": expr_stmt(node)
+      elif name in expr_types:
         reg = expr(node)
         free_reg(reg)
       else: exit("simple_stmt: Встречен неизвестый элемент: " + name)
@@ -259,6 +319,7 @@ while True:
 print(C)
 """
 
+# успешно прошедший код:
 code = """
 input()
 print(input())
@@ -266,4 +327,23 @@ print input() # эффект того, что lib2to3 одновеременно
 print 11
 print(7)
 """
+
+# разработка:
+code = """
+B; A; D # так можно манипулировать порядком "констант", что занимают эти переменные
+A = input()
+B = input()
+print(B)
+print(A)
+print(C) # 0
+print(D) # 0
+A += 11; print(A)
+B -= 5; print(B)
+A *= 7; print(A)
+A /= 3; print(A)
+A %= 123; print(A)
+C = (A + B + A) * 10 - 5
+print(C / 123); print(C % 123)
+"""
+
 compiler(code)
